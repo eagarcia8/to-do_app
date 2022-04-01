1a2dc73574129defb20497b1dd62cb14305fbc2c
Eduardo <medatech@medasf.org>
2022-04-01 12:54:33 -0700

    Updated ToDo

--- todo/index.js
+++ todo/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const req = require("express/lib/request");
 
 const app = express();
 
@@ -10,5 +12,49 @@ http.listen(port);
 
 console.log(`Express server is running on port ${port}.`);
 
+// Use body-parser to convert our front-end data into JavaScript Objects.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Custom Variables
+const tasks = [];
+
 // Express Routes
 app.use("/", express.static("public_html/"));
+
+// POST Handlers
+app.post("/createTask", function (request, response) {
+
+    let newTask = request.body;
+
+    if (newTask.description === "") {
+        let message = {
+            message: "You need to have a value for the Task Description.",
+            error: true
+        };
+
+        response.send(message);
+    } else {
+
+        console.log(newTask);
+
+        tasks.push(newTask);
+
+        let message = {
+            message: "Task added successfully!",
+            error: false
+        };
+        
+        // .send() ends the function, make sure its the last line in our function.
+        response.send(message);
+    }
+});
+
+app.post("/list", function (request, response) {
+    
+    let responseObject = {
+        list: tasks
+    }
+
+    response.send(responseObject);
+});
